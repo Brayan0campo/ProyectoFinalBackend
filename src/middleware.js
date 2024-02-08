ba3995082a64a785f1c292e188c2e54db4b7f3fb
Brayan <ocamporodriguezbrayan@gmail.com>
2024-02-07 23:57:09 -0500

    Update logger assignment in loggerMiddleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,6 @@ import { logger } from "./logger.js";
 
 // Middleware to add the logger to the request object
 export const loggerMiddleware = function (req, res, next) {
-  logger = logger;
+  req.logger = logger;
   next();
 };
